f253f077b8bdd67dfe2dcc79eb447ccc42de782c
AxelDeJesuuHernandezCoronado <xlhernandezcoronado@gmail.com>
2026-06-26 09:23:48 -0600

    Agregar archivos de proyecto.

---
+++ Vehiculo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estacionamiento2._0
+{
+    internal abstract class vehiculo
+    {
+        public float HorasEstacionado { get; set; }
+
+        public vehiculo(float horasestacionado)
+        {
+            HorasEstacionado = horasestacionado;
+        }
+
+        public abstract float CalcularTarifa();
+    }
+}
