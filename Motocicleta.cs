0b72f87853f0942fff274459f8ebeb825c7ca90f
Yesenia Magdalena <yesenia21th55@gmail.com>
2026-06-26 09:49:57 -0600

    Crear la subclase Motocicleta

---
+++ Motocicleta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estacionamiento2._0
+{
+    internal class Motocicleta : vehiculo
+    {
+        public int cilindrada;
+        public decimal tarifaFija = 15.0m;
+        private decimal horasestacionado;
+
+        
+
+        public Motocicleta(float horasestacionado) : base(horasestacionado)
+        {
+            HorasEstacionado = horasestacionado;
+        }
+
+        public override float CalcularTarifa()
+        {
+            return (float)(horasestacionado * tarifaFija);
+        }
+    }
+}
